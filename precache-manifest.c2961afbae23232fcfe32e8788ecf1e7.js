dca02fc07dd4ba73750e55058a9867ffb39dbcfe
pelegred <pelegred@gmail.com>
2020-10-15 19:45:15 +0300

    Updates

---
+++ precache-manifest.c2961afbae23232fcfe32e8788ecf1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6387008d6b5059574bd471e84f71d2e",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2337aaa64df933603eba",
+    "url": "/react-todo/static/css/main.e7201c15.chunk.css"
+  },
+  {
+    "revision": "d9b4242d774512631806",
+    "url": "/react-todo/static/js/2.94158880.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/react-todo/static/js/2.94158880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2337aaa64df933603eba",
+    "url": "/react-todo/static/js/main.b2f64f84.chunk.js"
+  },
+  {
+    "revision": "b81e150b308975bc2389",
+    "url": "/react-todo/static/js/runtime-main.89ae8f88.js"
+  }
+]);
